30dfd7aface477496f66902fc5900f8aeb30b284
Zachary Schulz <zjschulz@gmail.com>
2019-09-22 18:45:25 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,6 +18,8 @@ def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
       contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+        end
       end
     end
   end
